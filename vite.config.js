90a792185d4cbed0f9cb1e9c338db0632435e3d1
vudesign116 <69900815+vudesign116@users.noreply.github.com>
2025-10-06 16:37:53 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pingme-ai-assistant/', // Change this to your repo name
+  base: '/pingme/', // Change this to your repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
